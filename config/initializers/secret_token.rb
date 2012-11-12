63491578e72708c1517255e9c1e06a2b1f60327d
speric <eric@prudentiadigital.com>
2012-11-12 14:15:43 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StatusApp::Application.config.secret_token = 'c64a1a9baa753934cdda716f9c061245c4261f7c809a2868d58102602de0f583a1637076001af399c99d488c66072dc79b7bdf5edf1b309c34fd1f365ce9d52e'
